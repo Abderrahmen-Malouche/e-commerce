d5be20c9fca3332a0d5efaa2d8019a326a118b30
Abderrahmen-Malouche <121557323+Abderrahmen-Malouche@users.noreply.github.com>
2025-02-20 21:38:16 +0100

    f

--- backend/server.js
+++ backend/server.js
@@ -6,30 +6,23 @@ import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoutes.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
-const app=express();
+const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 connectCloudinary();
 //middlewares
 app.use(express.json());
-app.use(cors(
-    {
-        origin:"https://forever-frontend-eight-wheat.vercel.app/",
-        methods:"GET,POST,PUT,DELETE",
-        credentials:true
-    }
-));
+app.use(cors());
 
 //api endpoints
-app.use("/api/user",userRouter);
-app.use("/api/product",productRouter);
-app.use("/api/cart",cartRouter);
-app.use("/api/order",orderRouter);
-app.get("/",(req,res)=>{
-    res.send("Welcome to the server");
-})
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+app.get("/", (req, res) => {
+  res.send("Welcome to the server");
+});
 
-app.listen(PORT,()=>{console.log("Server is running on port",PORT)});
-
-
-
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
