bed87f884b7af43dc554c5955182859878462f17
Abderrahmen-Malouche <121557323+Abderrahmen-Malouche@users.noreply.github.com>
2025-06-30 13:19:15 +0200

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
         <div>
 
         <hr className='h-[1px] bg-gray-200'/>
-        <p className='text-center font-medium text-sm py-8 '>Copyright 2024@ abderrahmenmalouche- All Right Reserved.</p>
+        <p className='text-center font-medium text-sm py-8 '>Copyright 2024@ louay- All Right Reserved.</p>
         </div>
     </footer>
   )
